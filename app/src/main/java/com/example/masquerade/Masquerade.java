cad3e82a8f7e4e65c1bd9956cf5c72a35fb8abfc
wez162 <wez162@ucsd.edu>
2019-05-06 09:35:46 -0700

    added launch page, login page, homepage

--- app/src/main/java/com/example/masquerade/Masquerade.java
+++ app/src/main/java/com/example/masquerade/Masquerade.java
@@ -4,16 +4,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toolbar;
 
 public class Masquerade extends AppCompatActivity {
-    private Button SignUp;
-    private Button LogIn;
-    private EditText email;
-    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
