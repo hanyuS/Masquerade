94552cf956a8bfc148278462a862d0462d2faf8d
Charles-Lu <iamcharleslu@gmail.com>
2019-05-07 01:08:31 -0700

    implement drawer of homepage
    1. Create HomeActivity class
    2. Edit res\values\Styles and Strings to remove stupid default top bar and prevent bugs

---
+++ app/src/main/java/com/example/masquerade/HomeActivity.java
@@ -0,0 +1,45 @@
+package com.example.masquerade;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ListView navigation = findViewById(R.id.nav_list);
+        final String activity_title = getTitle().toString();
+        final String drawer_title = "About Me";
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.drawer_open, R.string.drawer_close){
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(activity_title);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(drawer_title);
+                invalidateOptionsMenu();
+            }
+        };
+
+        drawer.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(true);
+        toggle.syncState();
+    }
+}
