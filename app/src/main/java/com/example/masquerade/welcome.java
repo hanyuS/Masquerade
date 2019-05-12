862bb944856892d6133fc705c8ff7c9151ddb562
Charles-Lu <iamcharleslu@gmail.com>
2019-05-11 19:15:42 -0700

    finalize welcome page

--- app/src/main/java/com/example/masquerade/welcome.java
+++ app/src/main/java/com/example/masquerade/welcome.java
@@ -1,32 +1,30 @@
 package com.example.masquerade;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.app.ActivityOptions;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Looper;
-import android.support.design.widget.*;
-import android.support.v4.app.ActivityOptionsCompat;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.view.View;
-import android.view.Window;
-import android.view.animation.*;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.os.Handler;
 
 
-import java.security.ProtectionDomain;
-import java.util.Timer;
-import java.util.TimerTask;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 import static android.app.PendingIntent.getActivity;
 
 public class welcome extends AppCompatActivity {
-    ImageView logo;
-    Handler handler = new Handler();
+    ConstraintLayout logo_layout;
+    ConstraintLayout welcome;
+    ConstraintLayout signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,30 +33,73 @@ public class welcome extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        if (getSupportActionBar() != null){
+        if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
         setContentView(R.layout.activity_welcome);
 
-        logo = findViewById(R.id.logo);
+        logo_layout = findViewById(R.id.logo_layout);
+        welcome = findViewById(R.id.welcome);
+        signin = findViewById((R.id.signin));
 
-       handler.postDelayed(new Runnable(){
-           @Override
-           public void run() {
-               Intent loginIntent = new Intent(welcome.this,login.class);
-               startActivity(loginIntent);
-               overridePendingTransition(0, 0);
-               finish();
-               //overridePendingTransition(0, 0);
-           }
-       }, 3000);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        final DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        final int height = displayMetrics.heightPixels;
+
+        final ImageButton start = findViewById(R.id.start);
+        start.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.pulse));
+
+        final Animation out_anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
+        final Animation in_anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+
+        out_anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                signin.startAnimation(in_anim);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+
+        in_anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                signin.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {}
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ObjectAnimator animation = ObjectAnimator.ofFloat(welcome, "translationY", height);
+                animation.setDuration(1000);
+                animation.start();
+                logo_layout.startAnimation(out_anim);
+                start.clearAnimation();
+                logo_layout.removeView(start);
+            }
+        });
 
     }
 
     @Override
     protected void onPause() {
-        overridePendingTransition(0,0);
+        overridePendingTransition(0, 0);
         super.onPause();
     }
 }
