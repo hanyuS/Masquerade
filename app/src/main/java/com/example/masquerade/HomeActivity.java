3398d2c6f8a48295b2fc75943089469f34b61a9d
shuaishuaixiang <ziz358@ucsd.edu>
2019-05-30 18:06:13 -0700

    some changes

--- app/src/main/java/com/example/masquerade/HomeActivity.java
+++ app/src/main/java/com/example/masquerade/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.masquerade;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.support.design.widget.FloatingActionButton;
@@ -46,10 +47,12 @@ public class HomeActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             int times = 0;
             public void onClick(View v) {
+                /*
                 FloatingActionButton fab = findViewById(R.id.match);
                 if (times == 0) {
                     fab.setImageResource(R.drawable.rotate);
                     times = 1;
+                    startActivity(new Intent(HomeActivity.this, PairContact.class));
                 }
                 else if (times == 1){
                     fab.setImageResource(R.drawable.logo_small);
@@ -60,7 +63,10 @@ public class HomeActivity extends AppCompatActivity {
                     fab.setImageResource(R.drawable.ic_add_black_24dp);
                     fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorAccent)));
                     times = 0;
-                }
+                }*/
+                Log.d("start pair", "start pair");
+                startActivity(new Intent(HomeActivity.this, PairContact.class));
+
             }
         });
 
@@ -69,10 +75,10 @@ public class HomeActivity extends AppCompatActivity {
         toggle.syncState();
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.d("tags", "jumping into pairContact");
-        startActivity(new Intent(HomeActivity.this, PairContact.class));
-    }
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        Log.d("tags", "jumping into pairContact");
+//        startActivity(new Intent(HomeActivity.this, PairContact.class));
+//    }
 }
